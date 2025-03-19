ee4008320b90942ce252d1e74abfab97570f1ba8
tharun067 <tharunchava126@gmail.com>
2025-03-19 20:58:33 +0530

    message backend

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const getUserForSidebar = async (req, res) => {
     try {
@@ -16,9 +17,49 @@ export const getUserForSidebar = async (req, res) => {
 export const getMessages = async (req, res) => {
     try {
         const { id: userToChatId } = req.params;
-        const senderId = req.user._id;
-        const messages = await Message.find({})
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+            $or: [
+                { senderId: myId, receiverId: userToChatId },
+                {senderId:userToChatId,receiverId:myId}
+            ]
+        })
+
+        res.status(200).json(messages);
     } catch (error) {
+        console.log("Error in getMessage controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+
+export const sendMessage = async (req, res) => {
+    try {
+        const { text, image } = req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if (image) {
+            //Upload base64 image to cloudinary
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl,
+        });
+
+        await newMessage.save();
         
+        //todo:realtime functionality goes here => socket.io
+
+        res.status(201).json(newMessage);
+    } catch (error) {
+        console.log("Error in sendMessage controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
     }
 }
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getMessages, getUserForSidebar } from "../controllers/message.controller.js";
+import { getMessages, getUserForSidebar, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/user", protectRoute, getUserForSidebar);
 router.get("/:id", protectRoute, getMessages);
+
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
